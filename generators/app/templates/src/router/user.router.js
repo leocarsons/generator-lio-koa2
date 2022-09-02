c2b271beaa6517fabf4e09c6a63ca0fb3030fd43
刘传 <2447629916@qq.com>
2022-09-02 15:32:51 +0800

    升级

--- generators/app/templates/src/router/user.router.js
+++ generators/app/templates/src/router/user.router.js
@@ -5,7 +5,7 @@ const { userValidator, verifyUser, cryptPassword, verifyLogin } = require('../mi
 const validate=require('koa2-validation')
 // 挂载user的控制器
 const { login, register, updatePassword } = require('../controller/user.controller');
-const res = require('express/lib/response');
+// const res = require('express/lib/response');
 user.post('/login', userValidator, verifyLogin, login);
 user.post('/register', userValidator, verifyUser, cryptPassword, register);
 
